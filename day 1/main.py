f18b103e36668987d6faa6e08670723d790ffb0f
Dominik Wolny <wolny.dominik@outlook.com>
2019-12-03 14:53:38 +0100

    Task 2 of day 1 finished

--- day 1/main.py
+++ day 1/main.py
@@ -15,4 +15,30 @@ def task1():
             result += tempResult
             lineText = file.readline()
 
-    print(result)
+    return result
+
+
+def task2():
+
+    filepath = 'input.txt'
+    result = 0
+
+    with open(filepath) as file:
+        lineText = file.readline()
+        while lineText:
+            tempResult = int(lineText)
+            while tempResult > 0:
+                tempResult = math.floor(tempResult/3)
+                tempResult -= 2
+                result += tempResult
+            result -= tempResult
+            lineText = file.readline()
+
+    return result
+
+
+if __name__ == '__main__':
+    print('Task 1:')
+    print("\t{}\n".format(task1()))
+    print('Task 2:')
+    print("\t{}\n".format(task2()))
