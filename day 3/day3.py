4ada4a71bff38f52ff8306d74a796c2179702b01
Dominik Wolny <wolny.dominik@outlook.com>
2019-12-11 12:19:30 +0100

    Task 2 of day 3 finished

--- day 3/day3.py
+++ day 3/day3.py
@@ -59,7 +59,7 @@ def crosses(a1: [], a2: [], b1: [], b2: []):
     return False
 
 
-def find_traces(cable: []):
+def find_full_traces(cable: []):
     trace = []
     trace.append([0,0])
     for i in range(0, len(cable)):
@@ -84,17 +84,71 @@ def find_traces(cable: []):
     return trace
 
 
-def calc_crossings(trace_1: [], trace_2: []):
+def find_trace_len_to_point(cable: [], point: ()):
+    trace_len = 0
+    loc = [0,0]
+    for i in range(0, len(cable)):
+        if cable[i][0] == 'U':
+            for j in range(0, int(cable[i][1:])):
+                loc[1] += 1
+                trace_len += 1
+                if loc[0] == point[0] and loc[1] == point[1]:
+                    #print('to point: {}, {}: {}\n'.format(point[0], point[1], trace_len))
+                    return trace_len
+        elif cable[i][0] == 'D':
+            for j in range(0, int(cable[i][1:])):
+                loc[1] -= 1
+                trace_len += 1
+                if loc[0] == point[0] and loc[1] == point[1]:
+                    #print('to point: {}, {}: {}\n'.format(point[0], point[1], trace_len))
+                    return trace_len
+        elif cable[i][0] == 'L':
+            for j in range(0, int(cable[i][1:])):
+                loc[0] -= 1
+                trace_len += 1
+                if loc[0] == point[0] and loc[1] == point[1]:
+                    #print('to point: {}, {}: {}\n'.format(point[0], point[1], trace_len))
+                    return trace_len
+        elif cable[i][0] == 'R':
+            for j in range(0, int(cable[i][1:])):
+                loc[0] += 1
+                trace_len += 1
+                if loc[0] == point[0] and loc[1] == point[1]:
+                    #print('to point: {}, {}: {}\n'.format(point[0], point[1], trace_len))
+                    return trace_len
+        
+        #return trace_len
+    
+    return -1
+
+
+#def find_trace_len_to_point(cable: [], point: int):
+#    trace_len = 0
+#    
+#    for i in range(0, point):
+#        trace_len += int(cable[i][1:])
+#    
+#    print(trace_len)
+#    return trace_len
+
+
+def calc_crossings(trace_1: [], trace_2: [], ij: bool):
     crossings = []
     for i in range(1, len(trace_1)):
         for j in range(1, len(trace_2)):
             if crosses(trace_1[i-1], trace_1[i], trace_2[j-1], trace_2[j]):
-                crossings.append([trace_1[i][0], trace_2[j][1]])
+                if ij:
+                    crossings.append([1, trace_1[i][0], trace_2[j][1]])
+                else:
+                    crossings.append([trace_1[i][0], trace_2[j][1]])
 
     for i in range(1, len(trace_2)):
         for j in range(1, len(trace_1)):
             if crosses(trace_2[i-1], trace_2[i], trace_1[j-1], trace_1[j]):
-                crossings.append([trace_2[i][0], trace_1[j][1]])
+                if ij:
+                    crossings.append([2, trace_2[i][0], trace_1[j][1]])
+                else:
+                    crossings.append([trace_2[i][0], trace_1[j][1]])
 
     ### DEBUG ###
 
@@ -103,11 +157,36 @@ def calc_crossings(trace_1: [], trace_2: []):
     return crossings
 
 
-def task_1():
-    trace_cable_1 = find_traces(cable_1)
-    trace_cable_2 = find_traces(cable_2)
+#def calc_crossings(trace_1: [], trace_2: [], ij: bool):
+#    crossings = []
+#    for i in range(1, len(trace_1)):
+#        for j in range(1, len(trace_2)):
+#            if crosses(trace_1[i-1], trace_1[i], trace_2[j-1], trace_2[j]):
+#                if ij:
+#                    crossings.append([1, i, j])
+#                else:
+#                    crossings.append([trace_1[i][0], trace_2[j][1]])
+#
+#    for i in range(1, len(trace_2)):
+#        for j in range(1, len(trace_1)):
+#            if crosses(trace_2[i-1], trace_2[i], trace_1[j-1], trace_1[j]):
+#                if ij:
+#                    crossings.append([2, i, j])
+#                else:
+#                    crossings.append([trace_2[i][0], trace_1[j][1]])
+#
+#    ### DEBUG ###
+#
+#    #print(crossings)        
+#
+#    return crossings
 
-    crossings = calc_crossings(trace_cable_1, trace_cable_2)
+
+def task_1():
+    trace_cable_1 = find_full_traces(cable_1)
+    trace_cable_2 = find_full_traces(cable_2)
+
+    crossings = calc_crossings(trace_cable_1, trace_cable_2, False)
         
     lengths = []
     for cross in crossings:
@@ -127,9 +206,104 @@ def task_1():
 
 
 def task_2():
-    pass
+    trace_cable_1 = find_full_traces(cable_1)
+    trace_cable_2 = find_full_traces(cable_2)
+
+    crossings = calc_crossings(trace_cable_1, trace_cable_2, True)
+
+    lengths = []
+
+    for cross in crossings:
+        if cross[0] == 1:
+            lengths.append(find_trace_len_to_point(cable_1, (cross[1], cross[2])) + find_trace_len_to_point(cable_2, (cross[1], cross[2])))
+        elif cross[0] == 2:
+            lengths.append(find_trace_len_to_point(cable_2, (cross[1], cross[2])) + find_trace_len_to_point(cable_1, (cross[1], cross[2])))
+
+    #print(lengths)
+
+    ret = 0
+    for i in range(0, len(lengths)):
+        if lengths[i] < lengths[ret]:
+            ret = i
+
+    print(lengths[ret])
 
 
 if __name__ == '__main__':
     load_cables()
-    task_1()
+    #task_1()
+    task_2()
+
+
+#def find_full_traces(cable: []):
+#    trace = []
+#    trace.append([0,0])
+#    for i in range(0, len(cable)):
+#        if cable[i][0] == 'U':
+#            trace.append([trace[i][0], trace[i][1]+int(cable[i][1:])])
+#        elif cable[i][0] == 'D':
+#            trace.append([trace[i][0], trace[i][1]-int(cable[i][1:])])
+#        elif cable[i][0] == 'L':
+#            trace.append([trace[i][0]-int(cable[i][1:]), trace[i][1]])
+#        elif cable[i][0] == 'R':
+#            trace.append([trace[i][0]+int(cable[i][1:]), trace[i][1]])
+#
+#    ### DEBUG PRINTS ###
+#
+#    #print('trace cable 1')
+#    #for i in trace_cable_1:
+#    #    print(i)
+#    #print('trace cable 2')
+#    #for i in trace_cable_2:
+#    #    print(i)
+#
+#    return trace
+#
+
+#
+#
+#def task_1():
+#    trace_cable_1 = find_full_traces(cable_1)
+#    trace_cable_2 = find_full_traces(cable_2)
+#
+#    crossings = calc_crossings(trace_cable_1, trace_cable_2, False)
+#        
+#    lengths = []
+#    for cross in crossings:
+#        lengths.append(abs(cross[0])+abs(cross[1]))
+#    
+#    ### DEBUG PRINTS ###
+#
+#    #print (lengths)
+#    #print(crossings)
+#
+#    res = 0
+#    for i in range(0, len(lengths)):
+#        if lengths[i] < lengths[res]:
+#            res = i
+#
+#    print(lengths[res])
+#
+#
+#def task_2():
+#    trace_cable_1 = find_full_traces(cable_1)
+#    trace_cable_2 = find_full_traces(cable_2)
+#
+#    crossings = calc_crossings(trace_cable_1, trace_cable_2, True)
+#
+#    lengths = []
+#
+#    for cross in crossings:
+#        if cross[0] == 1:
+#            lengths.append(find_trace_len_to_point(cable_1, cross[0]) + find_trace_len_to_point(cable_2, cross[1]))
+#        elif cross[0] == 2:
+#            lengths.append(find_trace_len_to_point(cable_2, cross[0]) + find_trace_len_to_point(cable_1, cross[1]))
+#
+#    ret = 0
+#    for i in range(0,len(lengths)):
+#        if lengths[i] < lengths[ret]:
+#            ret = i
+#
+#    print(lengths[i])
+#
+#
